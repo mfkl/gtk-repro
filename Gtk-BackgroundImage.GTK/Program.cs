7cd90a9a81295e1bac6c77f488dd8f0f324492ee
Martin Finkel <martin@videolabs.io>
2019-06-08 15:54:51 +0700

    add gtk repro

---
+++ Gtk-BackgroundImage.GTK/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms.Platform.GTK;
+
+namespace Gtk_BackgroundImage.GTK
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            global::Gtk.Application.Init();
+            
+            global::Xamarin.Forms.Forms.Init();
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("GTK# sample");
+            window.Show();
+
+            global::Gtk.Application.Run();
+        }
+    }
+}
---
+++ Gtk-BackgroundImage/Gtk-BackgroundImage/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Gtk_BackgroundImage
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
